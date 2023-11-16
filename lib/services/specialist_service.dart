40f2cd72af8af3c231c1a4b8d6ae2ca116b13725
Bruno <u201914974@upc.edu.pe>
2023-11-16 08:04:06 -0500

    update service specialist

--- lib/services/specialist_service.dart
+++ lib/services/specialist_service.dart
@@ -6,29 +6,33 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 class SpecilistService {
-  static Future<List<Specialist>> getSpecilist() async {
+  static Future<List<Specialist>> getSpecilists() async {
     var url = Uri.parse('http://nifty-jet-404014.rj.r.appspot.com/api/v1/specialists');
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('token');
+    final farmerId = prefs.getInt('accountId');
+    var userUrl = Uri.parse('http://nifty-jet-404014.rj.r.appspot.com/api/v1/crops/$farmerId');
     Map<String, String> headers = {
       "Accept": "application/json",
       "Content-Type": "application/json",
-      "Authorization": 'Bearer $token'
+     // "Authorization": 'Bearer $token'
     };
+    final responseUser = await http.get(userUrl, headers: headers); // Obtener el plantId de un Farmer
+    final userSpecialist = jsonDecode(responseUser.body); //json de los plantId
 
-    List<Specialist> specialistList = [];
+    List<Specialist2> specialistList = [];
 
-    final response = await http.get(url, headers: headers);
+    final response = await http.get(url, headers: headers); //retorna las plantas
 
     if (response.statusCode == 200) {
-      final specialists = jsonDecode(response.body);
-
+      final specialists = jsonDecode(response.body); // retorna los json de los specialistas
       for (var specialist in specialists) {
-        specialistList.add(Specialist.fromJson(specialist));
+        if (!userSpecialist.any((userSpecialist) => userSpecialist['specialistId'] == specialist['id'])) {
+          specialistList.add(Specialist2.fromJson(specialist));
+        }
       }
-
-      return specialistList;
     }
     return specialistList;
   }
+
+
 }
