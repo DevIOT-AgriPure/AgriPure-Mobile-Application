22c86e752d84795218f93b1c1ca3d7882a1015b1
Andres Reynoso <u202010196@upc.edu.pe>
2023-11-20 18:20:22 -0500

    Modified add plant view

--- lib/presentation/views/add_plant_view.dart
+++ lib/presentation/views/add_plant_view.dart
@@ -11,42 +11,73 @@ class AddPlantView extends StatefulWidget {
 }
 
 class _AddPlantViewState extends State<AddPlantView> {
-
   bool _isLoading = false;
   String searchText = '';
+
+  Future<void> _savePlant(int plantId) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+
+      await PlantService.savePlant(plantId);
+
+      // Operación exitosa, regresar con resultado verdadero.
+      Navigator.pop(context, true);
+    } catch (error) {
+      // Manejar errores específicos según sea necesario.
+      String errorMessage = "Ocurrió un error durante la operación";
+      errorMessage = error.toString();
+
+      Fluttertoast.showToast(
+        msg: errorMessage,
+        toastLength: Toast.LENGTH_SHORT,
+        backgroundColor: const Color.fromRGBO(255, 174, 0, 1.0),
+        textColor: Colors.black,
+        fontSize: 18.0,
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(40, 40, 40, 1.0),
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          elevation: 4,
-          iconTheme: IconThemeData(
+      backgroundColor: const Color.fromRGBO(40, 40, 40, 1.0),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 4,
+        iconTheme: const IconThemeData(
           color: Colors.white,
-          ),
-          title: Text('Add plants', style: TextStyle(
+        ),
+        title: const Text(
+          'Add plants',
+          style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
-            fontSize: 24
-          ),),
+            fontSize: 24,
+          ),
         ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Search plants",
+            const Text(
+              "Search plants",
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
-              ),),
-
-            SizedBox(
+              ),
+            ),
+            const SizedBox(
               height: 15,
             ),
-
             Padding(
               padding: const EdgeInsets.only(left: 5.0, right: 5.0),
               child: Container(
@@ -57,9 +88,9 @@ class _AddPlantViewState extends State<AddPlantView> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 10),
                   child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search a plant"
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search a plant",
                     ),
                     onChanged: (value) {
                       setState(() {
@@ -70,22 +101,25 @@ class _AddPlantViewState extends State<AddPlantView> {
                 ),
               ),
             ),
-
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
-
             FutureBuilder(
-                initialData: [],
-                future:  PlantService.getPlants(),
-                builder: (context, AsyncSnapshot<List> snapshot){
-                  List filteredPlants = snapshot.data!.where((plant) =>
-                      plant.name.toLowerCase().contains(searchText.toLowerCase())).toList();
-                  if (snapshot.data!.isEmpty) {
-                    return const Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Center(
-                        child: Text("All plants selected", style: TextStyle(
+              initialData: [],
+              future: PlantService.getPlants(),
+              builder: (context, AsyncSnapshot<List> snapshot) {
+                List filteredPlants = snapshot.data!
+                    .where((plant) => plant.name
+                        .toLowerCase()
+                        .contains(searchText.toLowerCase()))
+                    .toList();
+                if (snapshot.data!.isEmpty) {
+                  return const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Center(
+                      child: Text(
+                        "All plants selected",
+                        style: TextStyle(
                           fontSize: 18,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
@@ -93,132 +127,112 @@ class _AddPlantViewState extends State<AddPlantView> {
                       ),
                     ),
                   );
-                  } else {
-                    if (filteredPlants.isEmpty) {
-                      return const Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Center(
-                            child: Text("Plant not found", style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            ),
+                } else {
+                  if (filteredPlants.isEmpty) {
+                    return const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Text(
+                          "Plant not found",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
-                        );
-                    } else {
-                      return Expanded(
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Expanded(
                       child: ListView.builder(
-                          itemCount: filteredPlants.length,
-                          itemBuilder: (context, index) {
-                            var plant = filteredPlants[index];
+                        itemCount: filteredPlants.length,
+                        itemBuilder: (context, index) {
+                          var plant = filteredPlants[index];
 
-                            return Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(12.0),
-                                    child: Column(
+                          return Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: Image.network(
+                                        '${plant.imageUrl}',
+                                        width: double.infinity,
+                                        height: 200,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      "${plant.name}",
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
                                       children: [
-                                        ClipRRect(
-                                            borderRadius: BorderRadius.circular(
-                                                15),
-                                            child: Image.network(
-                                              '${plant.imageUrl}',
-                                              width: double.infinity,
-                                              height: 200,
-                                              fit: BoxFit.cover,)
-                                        ),
-
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-
-                                        Text("${plant.name}", style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 28,
-                                            fontWeight: FontWeight.bold
-                                        ),),
-
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-
-                                        Row(
-                                          children: [
-                                            Expanded(
-                                              child: ElevatedButton(
-                                                onPressed: () {
-                                                  setState(() {
-                                                    _isLoading = true;
-                                                  });
-
-                                                  PlantService.savePlant(
-                                                      plant.id).then((value) =>
-                                                      Navigator.pop(
-                                                          context, true)
-                                                  ).catchError((error) {
-                                                    String errorMessage = "Ocurrió un error durante el inicio de sesión";
-                                                    errorMessage =
-                                                        error.toString();
-                                                    Fluttertoast.showToast(
-                                                        msg: errorMessage,
-                                                        toastLength: Toast
-                                                            .LENGTH_SHORT,
-                                                        backgroundColor: const Color
-                                                            .fromRGBO(
-                                                            255, 174, 0, 1.0),
-                                                        textColor: Colors.black,
-                                                        fontSize: 18.0
-                                                    );
-                                                  }).whenComplete(() {
-                                                    setState(() {
-                                                      _isLoading = false;
-                                                    });
-                                                  });
-                                                },
-                                                style: ElevatedButton.styleFrom(
-                                                  backgroundColor: Colors
-                                                      .orange,
-                                                ),
-                                                child: _isLoading
-                                                    ? Container(
+                                        Expanded(
+                                          child: ElevatedButton(
+                                            onPressed: () =>
+                                                _savePlant(plant.id),
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: Colors.orange,
+                                            ),
+                                            child: _isLoading
+                                                ? const SizedBox(
                                                     height: 20,
                                                     width: 20,
-                                                    child: const CircularProgressIndicator(
-                                                      valueColor: AlwaysStoppedAnimation<
-                                                          Color>(Colors.white),
+                                                    child:
+                                                        CircularProgressIndicator(
+                                                      valueColor:
+                                                          AlwaysStoppedAnimation<
+                                                                  Color>(
+                                                              Colors.white),
                                                       strokeWidth: 3.0,
-                                                    )
-                                                ) : const Padding(
-                                                  padding: EdgeInsets.all(
-                                                      12.0),
-                                                  child: Text(
-                                                    "Add", style: TextStyle(
-                                                      fontSize: 18,
-                                                      color: Colors.white,
-                                                      fontWeight: FontWeight
-                                                          .bold
-                                                  ),),
-                                                ),
-                                              ),
-                                            ),
-                                          ],
-                                        )
+                                                    ),
+                                                  )
+                                                : const Padding(
+                                                    padding:
+                                                        EdgeInsets.all(12.0),
+                                                    child: Text(
+                                                      "Add",
+                                                      style: TextStyle(
+                                                        fontSize: 18,
+                                                        color: Colors.white,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
+                                                    ),
+                                                  ),
+                                          ),
+                                        ),
                                       ],
-                                    ),
-                                  )
+                                    )
+                                  ],
+                                ),
                               ),
-                            );
-                          }),
+                            ),
+                          );
+                        },
+                      ),
                     );
-                    }
                   }
                 }
+              },
             )
           ],
         ),
